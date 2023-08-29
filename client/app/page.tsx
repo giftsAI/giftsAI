29a5a4e8c99c6a31e791ec82960f6414b35bc860
Yeong SilYoon <wendyys96@ucla.edu>
2023-08-29 00:06:38 -0700

    implemented callback function for form submission on the main page and created a recommendations component to disp lay the list of gift recommendations

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,22 +5,27 @@ import { useState } from 'react';
 import Recommendations from './recommendations';
 
 export default function Home(): JSX.Element {
-  const [recommendedGifts, setRecommendedGifts] = useState<any[]>([]);
-  const handleClick = async () : Promise<void> => {
-      const res: Response = await fetch('http://localhost:3500/gift/recommend', {
+  const [recommendedGifts, setRecommendedGifts] = useState<string[]>([]);
+
+  // form submission, submitting user's inputs and fetching gift recommendations from the server
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) : Promise<void> => {
+    event.preventDefault();
+    const target: EventTarget = event.currentTarget;
+    const data = {
+      receiver: target.receiver.value,
+      occasion: target.occasion.value,
+      interest: target.interest.value,
+      budget: target.budget.value
+    };
+    const res: Response = await fetch('http://localhost:3500/gift/recommend', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        body: JSON.stringify({
-          'receiver': 'wife',
-          'occasion': 'birthday',
-          'like': 'cooking',
-          'budget': '<$200'
-        })
-      }
-      });
-      const giftsArr = await res.json();
-      setRecommendedGifts(giftsArr);
+      },
+      body: JSON.stringify(data)
+    });
+    const giftsArr: string[] = await res.json();
+    setRecommendedGifts(giftsArr);
 
   }
   return (
@@ -50,13 +55,13 @@ export default function Home(): JSX.Element {
         </p> */}
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <form onSubmit={handleSubmit} className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <div
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-          rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>Who </h2>
+          <h2 className={`mb-3 text-2xl font-semibold`}>Who</h2>
           <input
+            id="receiver"
             placeholder="friend, spouse, etc."
             className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 transition-colors focus:outline-none focus:border-gray-500"
           ></input>
@@ -67,13 +72,13 @@ export default function Home(): JSX.Element {
 
         <div
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-          rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>Occasion </h2>
+          <h2 className={`mb-3 text-2xl font-semibold`}>Occasion</h2>
           <input
+            id="occasion"
             placeholder="birthday, graduation, etc."
             className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 transition-colors focus:outline-none focus:border-gray-500"
-          ></input>
+          />
           {/* <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Tips for occasion
           </p> */}
@@ -81,13 +86,13 @@ export default function Home(): JSX.Element {
 
         <div
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-          rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>Interests </h2>
+          <h2 className={`mb-3 text-2xl font-semibold`}>Interests</h2>
           <input
+            id="interest"
             placeholder="hobbies, activities, etc."
             className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30  transition-colors focus:outline-none focus:border-gray-500"
-          ></input>
+          />
           {/* <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Tips for interests
           </p> */}
@@ -95,22 +100,22 @@ export default function Home(): JSX.Element {
 
         <div
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors"
-          rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>Budget</h2>
           <input
+            id="budget"
             placeholder="$10, $50, $100, etc."
             className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30  transition-colors focus:outline-none focus:border-gray-500"
-          ></input>
+          />
           {/* <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Tips for budget
           </p> */}
         </div>
-      </div>
-
-      <button className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-sky-300 dark:text-black lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-sky-300 hover:bg-sky-200">
-        Give me ideas
-      </button>
+        <button className="border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-sky-300 dark:text-black lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-sky-300 hover:bg-sky-200">
+          Give me ideas
+        </button>
+      </form>
+      <Recommendations recommendedGifts={recommendedGifts}/>
     </main>
   );
 }
--- client/app/recommendations.tsx
+++ client/app/recommendations.tsx
@@ -1,18 +1,26 @@
 export default function Recommendations(props: {
-  recommendedGifts: any[]
+  recommendedGifts: string[]
 }) : JSX.Element {
-  const giftIdeas = props.recommendedGifts;
+  const giftIdeas: string[] = props.recommendedGifts;
   return (
-    <div>   
-      {
-        giftIdeas? <h2>Here are some suggestions:</h2> &&
-        <ul>
-        { giftIdeas.map((gift: string, index: number) : JSX.Element =>
-          <li key={gift + index.toString()}>{gift}</li>
-        )}
-        </ul> && 
-        <button>Save</button> : <></>
+    <div className="flex flex-col items-center justify-center w-full">   
+      { giftIdeas.length? <h2 className="mb-3 text-2xl font-semibold">Here are some suggestions:</h2> : <></>
       }
+      <dl className="w-full flex flex-col items-center justify-center m-4">
+        { giftIdeas.map((gift: string, index: number) : JSX.Element =>
+          <div className="p-4 m-2 w-3/5 border rounded-lg flex justify-between" key={gift + index.toString()}>
+            <dt>
+              {gift}
+            </dt>
+            <dd>
+              url
+            </dd>
+          </div>
+        )}
+      </dl>
+      {
+        giftIdeas.length? <button className="border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-sky-300 dark:text-black lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-sky-300 hover:bg-sky-200">Save</button> : <></>
+      } 
     </div>
   )
 };
