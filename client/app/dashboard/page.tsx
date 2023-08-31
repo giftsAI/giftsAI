28436f675cf42d755fac2d667fecfe86c7ef252f
Yeong SilYoon <wendyys96@ucla.edu>
2023-08-30 18:38:03 -0700

    working on search feature for past gift list

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,5 +1,113 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+
+interface Gift {
+  gift_id: number;
+  gifter_id: number;
+  receiver: string;
+  receiver_name: string;
+  occasion: string;
+  date: string;
+  interest: string;
+  budget: string;
+  gift: string;
+}
+
 function Dashboard(): JSX.Element {
-  return <>Dashboard page</>;
+  const [giftList, setGiftList] = useState<Gift[]>([]);
+  const [filteredList, setFilteredList] = useState<Gift[]>([]);
+  useEffect(() => {
+    setGiftList([{
+      gift_id: 1,
+      gifter_id: 2,
+      receiver: 'friend',
+      receiver_name: 'bailey',
+      occasion: 'birthday',
+      date: '10-23-2022',
+      interest: 'hiking',
+      budget: '$50',
+      gift: 'hiking shoes',
+    },
+    {
+      gift_id: 2,
+      gifter_id: 2,
+      receiver: 'friend',
+      receiver_name: 'mike',
+      occasion: 'birthday',
+      date: '11-02-2022',
+      interest: 'boardgames',
+      budget: '$50',
+      gift: 'monopoly',
+    }, 
+    {
+      gift_id: 3,
+      gifter_id: 2,
+      receiver: 'friend',
+      receiver_name: 'Sioban',
+      occasion: 'graduation',
+      date: '09-06-2020',
+      interest: 'reading',
+      budget: '$50',
+      gift: 'barnes and noble gift card',
+
+    }]);
+  }, []);  
+
+  const filterGifts = (e: React.FormEvent<HTMLInputElement>) : void => {
+    const receiver: string = e.currentTarget.value;
+    if(receiver === '') {
+      setFilteredList([]);
+      return;
+    }
+    setFilteredList(giftList.filter((gift) =>
+      gift.receiver_name.includes(receiver)
+    ));
+  };
+
+  return (
+    <main className="flex flex-col place-items-center justify-center lg:max-w-5xl lg:w-full lg:mb-0">
+      <h2 className="mb-3 text-2xl font-semibold">Past Gifts:</h2>
+      <input
+        placeholder="Search gifts by receiver's name"
+        onChange={filterGifts}
+        className="border-2 rounded-lg border-gray-300 bg-gradient-to-b from-zinc-200 px-8 py-6 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit w-full transition-colors focus:outline-none focus:border-gray-500"
+      ></input>
+      {
+        filteredList[0]? 
+        <div className="my-12 verflow-hidden w-full">
+          <table className="m-0 w-full">
+            <thead className="h-24 text-xl opacity-50">
+              <tr>
+                <th className="w-[14%]">Name</th>
+                <th className="w-[14%]">Relationship</th>
+                <th className="w-[14%]">Occasion</th>
+                <th className="w-[14%]">Interest</th>
+                <th className="w-[14%]">Budget</th>
+                <th className="w-[14%]">Gift</th>
+                <th className="w-[14%]">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                filteredList.map((gift: Gift) => (
+                  <tr key={gift.gift_id} className='h-24'>
+                    <th>{gift.receiver_name}</th>
+                    <th>{gift.receiver}</th>
+                    <th>{gift.occasion}</th>
+                    <th>{gift.interest}</th>
+                    <th>{gift.budget}</th>
+                    <th>{gift.gift}</th>
+                    <th>{gift.date}</th>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div> : <></>
+      }
+    </main>
+  );
 }
 
 export default Dashboard;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import Image from 'next/image';
 import { useState } from 'react';
 
-import Link from 'next/link';
-import Recommendations from './recommendations';
-import LoadingSpinner from './loadingSpinner';
+import Recommendations from './_components/recommendations';
+import LoadingSpinner from './_components/loadingSpinner';
 
 export default function Home(): JSX.Element {
   const [recommendedGifts, setRecommendedGifts] = useState<string[]>([]);
@@ -42,32 +40,7 @@ export default function Home(): JSX.Element {
     setLoading(false);
   };
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 space-y-40">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none space-y-40">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/logo.svg"
-              alt="giftsAI logo"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-        <Link
-          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-sky-300 dark:text-black lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-sky-300 hover:bg-sky-200"
-          href="/create-account"
-        >
-          Create Account
-        </Link>
-      </div>
-
+    <main>
       <form
         onSubmit={handleSubmit}
         className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"
