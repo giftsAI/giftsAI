b82460339071b0a6c5437267adbe91a781f1c8be
Yeong SilYoon <wendyys96@ucla.edu>
2023-09-01 20:56:47 -0700

    search is no longer case sensitive

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -62,7 +62,7 @@ function Dashboard(): JSX.Element {
       return;
     }
     setFilteredList(giftList.filter((gift) =>
-      gift.receiver_name.includes(receiver)
+      gift.receiver_name.toUpperCase().includes(receiver.toUpperCase())
     ));
   };
 
@@ -72,7 +72,7 @@ function Dashboard(): JSX.Element {
 
   return (
     <main className="flex flex-col place-items-center justify-center lg:max-w-5xl lg:w-full lg:mb-0">
-      <h2 className="mb-3 text-2xl font-semibold">Past Gifts:</h2>
+      <h2 className="mb-3 text-2xl font-semibold">Your Past Gifts:</h2>
       <input
         placeholder="Search gifts by receiver's name"
         onChange={filterGifts}
@@ -80,51 +80,53 @@ function Dashboard(): JSX.Element {
       ></input>
       {
         filteredList[0]? 
-        <div className="my-12 verflow-hidden w-full">
-          <table className="m-0 w-full">
+          <table className="my-12 w-full table-fixed">
             <thead className="h-24 text-xl opacity-50">
               <tr>
-                <th className="w-[12%]">Name</th>
-                <th className="w-[12%]">Relationship</th>
-                <th className="w-[12%]">Occasion</th>
-                <th className="w-[12%]">Interest</th>
-                <th className="w-[12%]">Budget</th>
-                <th className="w-[12%]">Gift</th>
-                <th className="w-[12%]">Date</th>
+                <th className="w-[13%]">Name</th>
+                <th className="w-[13%]">Relationship</th>
+                <th className="w-[13%]">Occasion</th>
+                <th className="w-[13%]">Interest</th>
+                <th className="w-[13%]">Budget</th>
+                <th className="w-[13%]">Gift</th>
+                <th className="w-[13%]">Date</th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="h-24">
               {
                 filteredList.map((gift: Gift) => (
-                  <tr key={gift.gift_id} className='h-24'>
-                    <th>{gift.receiver_name}</th>
-                    <th>{gift.receiver}</th>
-                    <th>{gift.occasion}</th>
-                    <th>{gift.interest}</th>
-                    <th>{gift.budget}</th>
-                    <th>{gift.gift}</th>
-                    <th>{gift.date}</th>
-                    <th>
+                  <tr key={gift.gift_id} className="h-24 hover:text-sky-300 text-center">
+                    <td>{gift.receiver_name}</td>
+                    <td>{gift.receiver}</td>
+                    <td>{gift.occasion}</td>
+                    <td>{gift.interest}</td>
+                    <td>{gift.budget}</td>
+                    <td>{gift.gift}</td>
+                    <td>{gift.date}</td>
+                    <td>
                       <button onClick={async () : Promise<void> => deleteGift(gift.gift_id)}>
                         <svg 
                           xmlns="http://www.w3.org/2000/svg" 
-                          fill="none" viewBox="0 0 24 24" 
+                          fill="none" 
+                          viewBox="0 0 24 24" 
                           strokeWidth={1.5} 
                           stroke="currentColor" 
-                          className="w-6 h-6 hover:text-red-600"
+                          className="w-6 h-6 hover:text-rose-700"
                         >
-                          <path strokeLinecap="round" 
+                          <path 
+                            strokeLinecap="round" 
                             strokeLinejoin="round" 
-                            d="M6 18L18 6M6 6l12 12" />
-                        </svg>
+                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" 
+                          />
+                        </svg>                       
                       </button>
-                    </th>
+                    </td>
                   </tr>
                 ))
               }
             </tbody>
           </table>
-        </div> : <></>
+          : <></>
       }
     </main>
   );
